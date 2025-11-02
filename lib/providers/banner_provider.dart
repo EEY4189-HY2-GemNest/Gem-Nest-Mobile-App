57d959d99ffab80783a1c9ea554a91821f68628c
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-02 11:29:49 +0530

    feat: implement fetchBannerImages method to load banner data from Firestore

--- lib/providers/banner_provider.dart
+++ lib/providers/banner_provider.dart
@@ -2,5 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BannerProvider extends ChangeNotifier {
-  
+  List<String> _bannerList = [];
+  bool _isLoading = false;
+  String? _error;
+
+  List<String> get bannerList => _bannerList;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+
+
 }
