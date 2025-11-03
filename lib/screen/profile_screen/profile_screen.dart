102e54c06829d282e4277869adb31d9ff0357474
thamara2X1 <vininduvtph@gmail.com>
2025-11-03 09:25:47 +0530

    feat: add ProfileScreen for user profile management

---
+++ lib/screen/profile_screen/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:gemnest_mobile_app/screens/auth_screens/login_screen.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+
