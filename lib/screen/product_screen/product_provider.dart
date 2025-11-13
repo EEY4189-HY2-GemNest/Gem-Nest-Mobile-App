975e12a80927942765f5d2dbe5dcec37153ba06d
thamara2X1 <vininduvtph@gmail.com>
2025-11-13 14:47:53 +0530

    feat: create Product model class for Firestore data handling

--- lib/screen/product_screen/product_provider.dart
+++ lib/screen/product_screen/product_provider.dart
@@ -1,2 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+// Define a product model for easier handling
+class Product {
+  final String title;
+  final double price;
+  final String imagePath;
+
+  Product({required this.title, required this.price, required this.imagePath});
+
+  // Convert document from Firestore to Product
+  factory Product.fromFirestore(DocumentSnapshot doc) {
+    var data = doc.data() as Map<String, dynamic>;
+    return Product(
+      title: data['title'],
+      price: data['price'],
+      imagePath: data['imagePath'],
+    );
+  }
+}
