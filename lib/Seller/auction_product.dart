d96d5fdfa3ccd886e0a32889fe6ceb8588f2a775
thamara2X1 <vininduvtph@gmail.com>
2025-11-24 20:56:46 +0530

    feat: implemented animation and input field widget in auction product screen

---
+++ lib/Seller/auction_product.dart
@@ -0,0 +1,88 @@
+
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _titleController.dispose();
+    _currentBidController.dispose();
+    _minimumIncrementController.dispose();
+    _endTimeController.dispose();
+    super.dispose();
+  }
+
+      body: SafeArea(
+        child: FadeTransition(
+          opacity: _animation,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField({
+    required String label,
+    required String hint,
+    required TextEditingController controller,
+    required String? Function(String?) validator,
+    int maxLines = 1,
+    TextInputType? keyboardType,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(color: Colors.white70, fontSize: 16),
+        ),
+        const SizedBox(height: 10),
+        TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey[900],
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.white54, fontSize: 14),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+              borderSide: const BorderSide(color: Colors.blue, width: 2),
+            ),
+            errorStyle: const TextStyle(color: Colors.red, fontSize: 12),
+          ),
+          maxLines: maxLines,
+          keyboardType: keyboardType,
+          style: const TextStyle(color: Colors.white, fontSize: 16),
+          validator: validator,
+        ),
+      ],
+    );
+  }
+
