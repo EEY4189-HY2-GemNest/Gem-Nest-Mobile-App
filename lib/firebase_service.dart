6b707d5e49f882c8343658d9a1c547907c7e4a5d
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-03 11:29:21 +0530

    feat(firebase): reintroduce addProduct method in FirebaseService for adding products to Firestore

--- lib/firebase_service.dart
+++ lib/firebase_service.dart
@@ -4,5 +4,16 @@ class FirebaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   // Method to add a product to Firestore
+  Future<void> addProduct(String title, double price, String imagePath) async {
+    try {
+      await _db.collection('products').add({
+        'title': title,
+        'price': price,
+        'imagePath': imagePath,
+      });
+    } catch (e) {
+      print("Error adding product: $e");
+    }
+  }
 
 }
