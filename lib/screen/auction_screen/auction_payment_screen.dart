463265717263923d59501444f9ec1f28932ebded
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-03 12:18:12 +0530

    feat(auction): enhance AuctionPaymentScreen with delivery options and payment controllers

--- lib/screen/auction_screen/auction_payment_screen.dart
+++ lib/screen/auction_screen/auction_payment_screen.dart
@@ -21,5 +21,35 @@ class AuctionPaymentScreen extends StatefulWidget {
   _AuctionPaymentScreenState createState() => _AuctionPaymentScreenState();
 }
 
+class _AuctionPaymentScreenState extends State<AuctionPaymentScreen> {
+  String _selectedDeliveryOption = 'pickup';
+  String? _selectedPaymentMethod;
+  final double _deliveryCharge = 1000.0;
+  bool _isLoading = false;
 
+  // Controllers for delivery details
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _postalCodeController = TextEditingController();
+
+  // Controllers for card payment
+  final TextEditingController _cardNumberController = TextEditingController();
+  final TextEditingController _expDateController = TextEditingController();
+  final TextEditingController _cvcController = TextEditingController();
+
+  String _formatCurrency(double amount) {
+    return 'Rs.${amount.toStringAsFixed(0).replaceAllMapped(
+          RegExp(r'(\d)(?=(\d{3})+(?!\d))'),
+          (Match m) => '${m[1]},',
+        )}';
+  }
+
+  double _calculateTotalPrice() {
+    return _selectedDeliveryOption == 'delivery'
+        ? widget.itemPrice + _deliveryCharge
+        : widget.itemPrice;
+  }
+
+  
 }
