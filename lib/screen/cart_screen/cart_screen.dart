bcbc22b215203aae54c30de7467fbaf1a5ebab1c
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-03 12:42:36 +0530

    feat(cart): add initial implementation of CartScreen

---
+++ lib/screen/cart_screen/cart_screen.dart
@@ -0,0 +1,15 @@
+// cart_screen.dart
+import 'package:flutter/material.dart';
+import 'package:gemnest_mobile_app/screens/checkout_screen/checkout_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'cart_provider.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  
+      },
+    );
+  }
+}
