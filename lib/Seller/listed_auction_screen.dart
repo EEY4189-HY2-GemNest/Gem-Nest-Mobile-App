c39f1dac992419faf147875b8d66aec49a47886c
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-05 01:57:53 +0530

    feat(auction-screen): add ListedAuctionScreen widget for displaying seller auctions

---
+++ lib/Seller/listed_auction_screen.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ListedAuctionScreen extends StatelessWidget {
+  final String sellerId;
+
+  const ListedAuctionScreen({super.key, required this.sellerId});
+
+  
+}
