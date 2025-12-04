dd7d24aa8579b8d0347f7468b1b1716eff1b9491
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-05 01:53:28 +0530

    feat(notifications): create NotificationsPage widget with delete and clear all callbacks

---
+++ lib/Seller/notifications_page.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart'; // Add this package for animations
+
+class NotificationsPage extends StatefulWidget {
+  final List<Map<String, dynamic>> notifications;
+  final Function(int)? onDelete; // Callback to delete a notification by index
+  final VoidCallback? onClearAll; // Callback to clear all notifications
+
+  const NotificationsPage({
+    super.key,
+    required this.notifications,
+    this.onDelete,
+    this.onClearAll,
+  });
+
+  @override
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
+
+
+}
