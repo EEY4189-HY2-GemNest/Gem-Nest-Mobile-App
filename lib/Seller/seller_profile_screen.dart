bf5299846cfd0c57f57248b05c8915c9a6038dae
thamara2X1 <vininduvtph@gmail.com>
2025-12-04 15:11:01 +0530

    feat(profile): add SellerProfileScreen with necessary imports and initial setup

---
+++ lib/Seller/seller_profile_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart'; // Added for animations
+import 'package:image_picker/image_picker.dart';
+
+class SellerProfileScreen extends StatefulWidget {
+  const SellerProfileScreen({super.key});
+
+  @override
+  State<SellerProfileScreen> createState() => _SellerProfileScreenState();
+}
+
