dd652aefe796c0cc75c596ffe5750acdf56e8a48
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-05 13:33:13 +0530

    feat: enhance CartItem model with additional properties and JSON serialization

--- lib/screen/cart_screen/cart_provider.dart
+++ lib/screen/cart_screen/cart_provider.dart
@@ -1,27 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class CartItem {
   final String id;
   final String imagePath;
   final String title;
   final double price;
+  final double originalPrice;
+  final String category;
+  final String sellerId;
+  final int availableStock;
+  final bool isDiscounted;
+  final double discountPercentage;
+  final Map<String, dynamic> productData;
   int quantity;
+  bool isSelected;
 
   CartItem({
     required this.id,
     required this.imagePath,
     required this.title,
     required this.price,
-    this.quantity = 1, // Default to 1, but allow override
+    required this.originalPrice,
+    required this.category,
+    required this.sellerId,
+    required this.availableStock,
+    required this.productData,
+    this.quantity = 1,
+    this.isSelected = true,
+    this.isDiscounted = false,
+    this.discountPercentage = 0.0,
   });
 
   double get totalPrice => price * quantity;
+  double get originalTotalPrice => originalPrice * quantity;
+  double get savings => originalTotalPrice - totalPrice;
+  
+  bool get isInStock => availableStock > 0;
+  bool get isQuantityAvailable => quantity <= availableStock;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'imagePath': imagePath,
+      'title': title,
+      'price': price,
+      'originalPrice': originalPrice,
+      'category': category,
+      'sellerId': sellerId,
+      'availableStock': availableStock,
+      'quantity': quantity,
+      'isSelected': isSelected,
+      'isDiscounted': isDiscounted,
+      'discountPercentage': discountPercentage,
+      'productData': productData,
+    };
+  }
+
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    return CartItem(
+      id: json['id'],
+      imagePath: json['imagePath'],
+      title: json['title'],
+      price: json['price'].toDouble(),
+      originalPrice: json['originalPrice'].toDouble(),
+      category: json['category'],
+      sellerId: json['sellerId'],
+      availableStock: json['availableStock'],
+      quantity: json['quantity'],
+      isSelected: json['isSelected'] ?? true,
+      isDiscounted: json['isDiscounted'] ?? false,
+      discountPercentage: json['discountPercentage']?.toDouble() ?? 0.0,
+      productData: json['productData'] ?? {},
+    );
+  }
 }
 
 class CartProvider with ChangeNotifier {
   final List<CartItem> _cartItems = [];
+  final List<CartItem> _wishlistItems = [];
+  String? _appliedCouponCode;
+  double _couponDiscount = 0.0;
+  double _shippingCost = 0.0;
+  double _taxRate = 0.1; // 10% tax
+  bool _isLoading = false;
 
+  // Getters
   List<CartItem> get cartItems => _cartItems;
+  List<CartItem> get wishlistItems => _wishlistItems;
+  List<CartItem> get selectedCartItems => _cartItems.where((item) => item.isSelected).toList();
+  String? get appliedCouponCode => _appliedCouponCode;
+  double get couponDiscount => _couponDiscount;
+  double get shippingCost => _shippingCost;
+  double get taxRate => _taxRate;
+  bool get isLoading => _isLoading;
+  
+  int get cartItemCount => _cartItems.fold(0, (sum, item) => sum + (item.isSelected ? item.quantity : 0));
+  int get wishlistItemCount => _wishlistItems.length;
 
   void addToCart(Map<String, dynamic> product) {
     final existingItemIndex =
