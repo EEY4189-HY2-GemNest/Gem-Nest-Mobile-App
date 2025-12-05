e09146cfeea1ea36d1fb15c254b3bb282d4f1de3
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-05 11:06:11 +0530

    fix: import ProfessionalAppBarBackButton in PaymentScreen

--- lib/screen/payment_screen/payment_screen.dart
+++ lib/screen/payment_screen/payment_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gemnest_mobile_app/screen/cart_screen/cart_provider.dart';
 import 'package:gemnest_mobile_app/screen/order_history_screen/oreder_history_screen.dart';
+import 'package:gemnest_mobile_app/widget/professional_back_button.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
