f93b2343b475d56a6444403c9a43bb9b77fc313a
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-08 16:57:51 +0530

    fix(payment): Change payment expanded state to mutable

--- lib/Seller/product_listing.dart
+++ lib/Seller/product_listing.dart
@@ -43,7 +43,7 @@ class _ProductListingState extends State<ProductListing>
   Map<String, Map<String, dynamic>> _availablePaymentMethods = {};
   final Set<String> _selectedPaymentMethods = {};
   bool _isLoadingPaymentConfig = true;
-  final bool _isPaymentExpanded = false;
+  bool _isPaymentExpanded = false;
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
