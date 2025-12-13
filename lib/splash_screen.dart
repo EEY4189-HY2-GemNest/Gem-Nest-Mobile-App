9236ad9bdb857fa08a83a1f0fe5e881af40ef2f1
MethminaYasas <methminay@gmail.com>
2025-12-13 20:22:58 +0530

    fix(splash):  unnecessary animation start in initState

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -32,7 +32,6 @@ class _SplashScreenState extends State<SplashScreen>
     );
 
     // Start the animation
-    _animationController.forward();
 
     // Navigate to the LoginScreen after 1 second
     Future.delayed(const Duration(seconds: 2), () {
