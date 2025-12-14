b385a3a94d444369d7949aa320a642544e2a3964
ParakramaWelipitiya <parakramawelipitiya00@gmail.com>
2025-12-14 19:45:28 +0530

    feat(cart_screen): implement animation controller and fade effect for cart screen

--- lib/screen/cart_screen/cart_screen.dart
+++ lib/screen/cart_screen/cart_screen.dart
@@ -14,3 +14,26 @@ class CartScreen extends StatefulWidget {
   @override
   State<CartScreen> createState() => _CartScreenState();
 }
+class _CartScreenState extends State<CartScreen> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _animationController.forward();
+
+    // Load cart from local storage - handled by CartProvider constructor now
+    Future.microtask(() {
+      context.read<CartProvider>().validateCartStock();
+    });
+  }
+
