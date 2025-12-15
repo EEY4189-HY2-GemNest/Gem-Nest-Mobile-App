c0d1b53c4e530f76549a865d42ccba39c95956fc
ParakramaWelipitiya <parakramawelipitiya00@gmail.com>
2025-12-15 22:15:57 +0530

    Add initial implementation of PaymentScreen with necessary imports

---
+++ lib/screen/payment_screen/payment_screen.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:gemnest_mobile_app/screen/cart_screen/cart_provider.dart';
+import 'package:gemnest_mobile_app/screen/checkout_screen/checkout_screen.dart'
+    as checkout;
+import 'package:gemnest_mobile_app/screen/order_history_screen/oreder_history_screen.dart';
+import 'package:gemnest_mobile_app/stripe_service_direct.dart';
+import 'package:gemnest_mobile_app/stripe_service_firebase.dart';
+import 'package:gemnest_mobile_app/theme/app_theme.dart';
+import 'package:gemnest_mobile_app/widget/professional_back_button.dart';
+import 'package:provider/provider.dart';
