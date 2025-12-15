72234502a32ad37899aa716fdf2c54f460e2d670
ParakramaWelipitiya <parakramawelipitiya00@gmail.com>
2025-12-15 21:47:14 +0530

    Add missing imports for cart_provider.dart

--- lib/screen/cart_screen/cart_provider.dart
+++ lib/screen/cart_screen/cart_provider.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CartItem {
   final String id;
