c42882e9508609712051457df215a2515271d7bf
thamara2X1 <vininduvtph@gmail.com>
2025-12-16 21:28:19 +0530

    feat(order-details): add state variables and controllers

--- lib/Seller/order_details_screen.dart
+++ lib/Seller/order_details_screen.dart
@@ -12,6 +12,20 @@ class OrderDetailsScreen extends StatefulWidget {
 }
 
 class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
+  final TextEditingController _deliveryDateController = TextEditingController();
+
+  String? _selectedStatus;
+
+  final List<String> _statusOptions = [
+    'Pending',
+    'Processing',
+    'Shipped',
+    'Delivered',
+    'Cancelled'
+  ];
+
+  late DocumentSnapshot orderData;
+  
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
