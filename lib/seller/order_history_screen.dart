1caa03c710c01262fbf7caf0749131e6b502d4a0
thamara2X1 <vininduvtph@gmail.com>
2025-12-17 22:04:03 +0530

    feat: add firebase auth and filter state variables

--- lib/seller/order_history_screen.dart
+++ lib/seller/order_history_screen.dart
@@ -1,3 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:gemnest_mobile_app/Seller/order_details_screen.dart';
+import 'package:gemnest_mobile_app/widget/professional_back_button.dart';
+import 'package:intl/intl.dart';
+
 class SellerOrderHistoryScreen extends StatefulWidget {
   const SellerOrderHistoryScreen({super.key});
 
@@ -6,4 +13,19 @@ class SellerOrderHistoryScreen extends StatefulWidget {
       _SellerOrderHistoryScreenState();
 }
 
-class _SellerOrderHistoryScreenState extends State<SellerOrderHistoryScreen> {}
+class _SellerOrderHistoryScreenState extends State<SellerOrderHistoryScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _selectedStatus = 'All';
+  String _sortBy = 'Date';
+  bool _isAscending = false;
+  DateTimeRange? _selectedDateRange;
+
+  final List<String> _statusOptions = [
+    'All',
+    'Pending',
+    'Processing',
+    'Shipped',
+    'Delivered',
+    'Cancelled'
+  ];
+  final List<String> _sortOptions = ['Date', 'Amount', 'Status'];
