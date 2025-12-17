f230100d08fdaa5c9c5d39e1cd65f59b9f9bfb66
PramodDissanayake <pramod.pradeeptha@gmail.com>
2025-12-17 12:20:52 -0500

    feat(storage): persist and restore login credentials using shared preferences

--- lib/screen/auth_screens/login_screen.dart
+++ lib/screen/auth_screens/login_screen.dart
@@ -20,7 +20,6 @@
 
 
 
-  bool rememberMe = false;
 
 
 
@@ -33,45 +32,29 @@
 
 
 
+  Future<void> _loadRememberedCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? savedEmail = prefs.getString('email');
+    String? savedPassword = prefs.getString('password');
 
+    if (savedPassword != null) {
+      setState(() {
+        emailController.text = savedEmail ?? '';
+        passwordController.text = savedPassword;
+        rememberMe = true;
+      });
+    }
+  }
 
+  Future<void> _saveCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (rememberMe) {
+      await prefs.setString('email', emailController.text.trim());
+      await prefs.setString('password', passwordController.text.trim());
+    } else {
+      await prefs.remove('email');
+      await prefs.remove('password');
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                          Checkbox(
-                            value: rememberMe,
-                            onChanged: (value) =>
-                                setState(() => rememberMe = value ?? false),
-                            activeColor: AppTheme.primaryBlue,
-                          ),
-                          
+  
