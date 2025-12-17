3ea3ae78d86c52b5d0ddb6a4b1a8689f8126cefb
PramodDissanayake <pramod.pradeeptha@gmail.com>
2025-12-17 11:42:58 -0500

    feat(validation): prevent login with empty email or password

--- lib/screen/auth_screens/login_screen.dart
+++ lib/screen/auth_screens/login_screen.dart
@@ -21,7 +21,6 @@
 
 
 
-  bool isLoading = false; 
 
 
 
@@ -59,64 +58,14 @@
 
 
 
-
-
-
-
-
-
-
-
-
-    setState(() => isLoading = true);
-
-    try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim(),
-      );
-
-      String? userId = userCredential.user?.uid;
-
-      DocumentSnapshot buyerSnapshot = await _firestore
-          .collection('buyers')
-          .doc(userId)
-          .get();
-      DocumentSnapshot sellerSnapshot = await _firestore
-          .collection('sellers')
-          .doc(userId)
-          .get();
-
-      if (buyerSnapshot.exists) {
-        await _saveCredentials();
-        _navigateTo(const HomeScreen());
-      } else if (sellerSnapshot.exists) {
-        Map<String, dynamic> sellerData =
-            sellerSnapshot.data() as Map<String, dynamic>;
-        if (!sellerData['isActive']) {
-          await _auth.signOut();
-          _showCustomDialog(
-            title: 'Account Disabled',
-            message:
-                'Your seller account is disabled. Please wait for Admin approval.',
-            isError: true,
-          );
-        } else {
-          await _saveCredentials();
-          _navigateTo(const SellerHomePage());
-        }
-      } else {
-        throw Exception('User role not found.');
-      }
-    } on FirebaseAuthException catch (e) {
+    if (emailController.text.trim().isEmpty ||
+        passwordController.text.trim().isEmpty) {
       _showCustomDialog(
-        title: 'Login Failed',
-        message: e.message ?? 'Login failed. Please try again.',
+        title: 'Error',
+        message: 'Please fill in all fields',
         isError: true,
       );
-    } catch (e) {
-      _showCustomDialog(title: 'Error', message: 'Error: $e', isError: true);
-    } finally {
-      setState(() => isLoading = false);
+      return;
     }
-  
+
+    
