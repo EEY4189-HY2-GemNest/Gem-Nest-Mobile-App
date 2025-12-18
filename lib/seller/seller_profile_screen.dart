c19c0dde9fe2b88872d8d2883ec521f022b06e66
thamara2X1 <vininduvtph@gmail.com>
2025-12-19 00:30:41 +0530

    feat: initialize Firebase services and base state variables

--- lib/seller/seller_profile_screen.dart
+++ lib/seller/seller_profile_screen.dart
@@ -10,5 +10,14 @@ class SellerProfileScreen extends StatefulWidget {
 
 class _SellerProfileScreenState extends State<SellerProfileScreen>
     with TickerProviderStateMixin {
-      
-    }
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  String? _profileImageUrl;
+  final ImagePicker _picker = ImagePicker();
+
+  // Seller data
+  Map<String, dynamic>? sellerData;
+  bool _isLoading = true;
+  bool _isUploadingProfilePic = false;
+}
