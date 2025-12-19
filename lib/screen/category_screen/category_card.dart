5dd921c38287e4ded593483e40336299069cf49e
MethminaYasas <methminay@gmail.com>
2025-12-19 20:36:15 +0530

    Add basic CategoryCard UI component for category display

--- lib/screen/category_screen/category_card.dart
+++ lib/screen/category_screen/category_card.dart
@@ -1,2 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gemnest_mobile_app/screen/category_screen/category_screen.dart';
+class CategoryCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+
