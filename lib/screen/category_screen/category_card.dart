7c5d88e2d3a5483b99e61ba30d7e28fbf273f36f
MethminaYasas <methminay@gmail.com>
2025-12-19 20:36:47 +0530

    Enable tap on CategoryCard to open corresponding CategoryScreen

--- lib/screen/category_screen/category_card.dart
+++ lib/screen/category_screen/category_card.dart
@@ -3,4 +3,18 @@ import 'package:gemnest_mobile_app/screen/category_screen/category_screen.dart';
 class CategoryCard extends StatelessWidget {
   final String imagePath;
   final String title;
+  const CategoryCard({super.key, required this.imagePath, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Navigate to the CategoryScreen with the category title
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryScreen(categoryTitle: title),
+          ),
+        );
+      },
 
