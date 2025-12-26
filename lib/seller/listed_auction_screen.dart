81b63e476dbc680281cbc6c8f4011a2fc1cc8f1e
thamara2X1 <vininduvtph@gmail.com>
2025-12-27 00:46:56 +0530

    feat: create ListedAuctionScreen widget skeleton

--- lib/seller/listed_auction_screen.dart
+++ lib/seller/listed_auction_screen.dart
@@ -2,3 +2,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class ListedAuctionScreen extends StatefulWidget {
+  final String sellerId;
+
+  const ListedAuctionScreen({super.key, required this.sellerId});
+
+  @override
+  State<ListedAuctionScreen> createState() => _ListedAuctionScreenState();
+}
+
+class _ListedAuctionScreenState extends State<ListedAuctionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          'Listed Auctions',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
