42312aa0da527b7839d3ab40b187e04196dd27f5
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-01-15 01:52:54 +0530

    fix(notification): remove unused Cloud Firestore import from BuyerNotificationProvider

--- lib/providers/buyer_notification_provider.dart
+++ lib/providers/buyer_notification_provider.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gemnest_mobile_app/models/notification_model.dart';
