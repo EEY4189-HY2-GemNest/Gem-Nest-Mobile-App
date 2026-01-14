b3ec503eb8573af99c4ac8fe136c6f65f284c685
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-01-15 02:07:09 +0530

    feat(checkout_screen): add ignore directive for deprecated_member_use and unused_field warnings

--- lib/screen/checkout_screen/checkout_screen.dart
+++ lib/screen/checkout_screen/checkout_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, unused_field
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
