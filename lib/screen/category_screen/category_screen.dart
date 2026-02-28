b60e53d94e249b4b0f0e1596cb57605935e0deb7
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-03-01 00:28:48 +0530

    fix: Correct import order for consistency in CategoryScreen

--- lib/screen/category_screen/category_screen.dart
+++ lib/screen/category_screen/category_screen.dart
@@ -2,8 +2,8 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:gemnest_mobile_app/widget/no_data_widget.dart';
 import 'package:gemnest_mobile_app/screen/product_screen/product_card.dart';
+import 'package:gemnest_mobile_app/widget/no_data_widget.dart';
 
 class CategoryScreen extends StatefulWidget {
   final String categoryTitle;
